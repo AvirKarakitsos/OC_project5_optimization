f6f5e454d21ab53969f268c8ce07c0233f444ac9
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-06-28 12:10:18 +0200

    feat: buttons for accessibility

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -79,7 +79,7 @@
     wrapItemInColumn(element, columns) {
       if (columns.constructor === Number) {
         element.wrap(
-          `<a href="#" class='item-column mb-4 col-${Math.ceil(12 / columns)}'></a>`
+          `<div class='item-column mb-4 col-${Math.ceil(12 / columns)}'></div>`
         );
       } else if (columns.constructor === Object) {
         var columnClasses = "";
@@ -98,7 +98,7 @@
         if (columns.xl) {
           columnClasses += ` col-xl-${Math.ceil(12 / columns.xl)}`;
         }
-        element.wrap(`<a href="#" class='item-column mb-4${columnClasses}'></a>`);
+        element.wrap(`<div class='item-column mb-4${columnClasses}'></div>`);
       } else {
         console.error(
           `Columns should be defined as numbers or objects. ${typeof columns} is not supported.`
@@ -205,13 +205,13 @@
                         <div class="modal-body">
                             ${
                               navigation
-                                ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
+                                ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><button aria-label="précédent"><</button></div>'
                                 : '<span style="display:none;" />'
                             }
                             <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
                             ${
                               navigation
-                                ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
+                                ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;"><button aria-label="suivant">></button></div>'
                                 : '<span style="display:none;" />'
                             }
                         </div>
